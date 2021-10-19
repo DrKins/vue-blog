0ebc6057bd0c3e3dc78c0a365ce09bead9c944c0
DrKins <fade.sestan@gmail.com>
2021-10-19 03:20:00 +0200

    feat(Main component with cards): Implemented main blog component. Simple info cards.

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <Content />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "./components/HelloWorld.vue";
+import Content from "./components/Content.vue";
 
 @Options({
   components: {
-    HelloWorld,
+    Content,
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Nunito", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #033;
+  background-color: #8ec5fc;
+  background-image: -webkit-linear-gradient(62deg, #8ec5fc 0%, #e0c3fc 100%);
+  background-image: -moz-linear-gradient(62deg, #8ec5fc 0%, #e0c3fc 100%);
+  background-image: -o-linear-gradient(62deg, #8ec5fc 0%, #e0c3fc 100%);
+  background-image: linear-gradient(62deg, #8ec5fc 0%, #e0c3fc 100%);
+  height: 100vh;
 }
 </style>
