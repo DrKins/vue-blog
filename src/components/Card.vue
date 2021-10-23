b64230925a051584de574bc4e53b88d330f01ffc
DrKins <fade.sestan@gmail.com>
2021-10-23 14:19:28 +0200

    perf(New component): Card component separated

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="TopQuote_Container" v-for="(item, index) in fetch" :key="index">
+    <div class="TopQuote">
+      <p class="Quote">"{{ item.quote }}"</p>
+      <div class="Reactions">
+        <div class="Likes">
+          <font-awesome-icon icon="heart" />
+          <p>{{ item.likes }}</p>
+        </div>
+        <div class="Dislikes">
+          <font-awesome-icon icon="heart-broken" />
+          <p>{{ item.dislikes }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+@Options({})
+export default class Card extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
