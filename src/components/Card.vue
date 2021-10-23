83debdc1b7a393d2c631aacfac80c6243061c238
DrKins <fade.sestan@gmail.com>
2021-10-23 14:32:36 +0200

    refactor(Separating code): Appropriate code added to new component, deleted from Landing

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="TopQuote_Container" v-for="(item, index) in fetch" :key="index">
+  <div class="TopQuote_Container" v-for="(item, index) in GetAPI" :key="index">
     <div class="TopQuote">
-      <p class="Quote">"{{ item.quote }}"</p>
+      <p class="Quote">"{{ item.text }}"</p>
       <div class="Reactions">
         <div class="Likes">
           <font-awesome-icon icon="heart" />
@@ -18,8 +18,74 @@
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-@Options({})
+import { mapGetters } from "vuex";
+@Options({
+  computed: {
+    ...mapGetters(["GetAPI"]),
+  },
+})
 export default class Card extends Vue {}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.TopQuote_Container {
+  .TopQuote {
+    margin: 2em 0;
+    padding: 1em 1em 0 1em;
+    text-align: left;
+    height: auto;
+    background: #ece9e6; /* fallback for old browsers */
+    background: -webkit-linear-gradient(
+      to right,
+      #ffffff,
+      #ece9e6
+    ); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(
+      to right,
+      #ffffff,
+      #ece9e6
+    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+    border-radius: 1em;
+    box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
+      rgba(0, 0, 0, 0.23) 0px 6px 6px;
+    .Quote {
+      font-style: italic;
+      font-weight: 500;
+      font-size: 1em;
+    }
+    .Reactions {
+      display: flex;
+      justify-content: space-evenly;
+      color: white;
+      padding: 1em 0;
+
+      .Likes {
+        display: flex;
+        width: 50%;
+        padding: 1em 2em;
+        justify-content: center;
+        align-items: center;
+        border-radius: 0.5em 0 0 0.5em;
+        background-image: linear-gradient(135deg, #81fbb8 10%, #28c76f 100%);
+        margin-right: 0.5em;
+        p {
+          margin-left: 0.5em;
+        }
+      }
+      .Dislikes {
+        display: flex;
+        padding: 1em 2em;
+        width: 50%;
+        justify-content: center;
+        align-items: center;
+        border-radius: 0 0.5em 0.5em 0;
+        background-image: linear-gradient(135deg, #feb692 10%, #ea5455 100%);
+        p {
+          margin-left: 0.5em;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,20 +1,6 @@
 <template>
   <div class="Landing_Container">
-    <div class="TopQuote_Container" v-for="(item, index) in fetch" :key="index">
-      <div class="TopQuote">
-        <p class="Quote">"{{ item.quote }}"</p>
-        <div class="Reactions">
-          <div class="Likes">
-            <font-awesome-icon icon="heart" />
-            <p>{{ item.likes }}</p>
-          </div>
-          <div class="Dislikes">
-            <font-awesome-icon icon="heart-broken" />
-            <p>{{ item.dislikes }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Card />
     <div class="ScrollButton animate__animated animate__fadeIn">
       <font-awesome-icon icon="chevron-circle-down" />
     </div>
@@ -23,7 +9,11 @@
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import Card from "./Card.vue";
 @Options({
+  components: {
+    Card,
+  },
   data() {
     return {
       fetch: [
@@ -65,74 +55,6 @@ $border: #5f9ea0;
 @media only screen and (min-device-width: 320px,) and (max-device-width: 480px,) {
   .Landing_Container {
     padding: 2em;
-    .TopQuote_Container {
-      .TopQuote {
-        margin: 2em 0;
-        padding: 1em 1em 0 1em;
-        text-align: left;
-        height: auto;
-        background: #ece9e6; /* fallback for old browsers */
-        background: -webkit-linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-        border-radius: 1em;
-        box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
-          rgba(0, 0, 0, 0.23) 0px 6px 6px;
-        .Quote {
-          font-style: italic;
-          font-weight: 500;
-          font-size: 1em;
-        }
-        .Reactions {
-          display: flex;
-          justify-content: space-evenly;
-          color: white;
-          padding: 1em 0;
-
-          .Likes {
-            display: flex;
-            width: 50%;
-            padding: 1em 2em;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0.5em 0 0 0.5em;
-            background-image: linear-gradient(
-              135deg,
-              #81fbb8 10%,
-              #28c76f 100%
-            );
-            margin-right: 0.5em;
-            p {
-              margin-left: 0.5em;
-            }
-          }
-          .Dislikes {
-            display: flex;
-            padding: 1em 2em;
-            width: 50%;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0 0.5em 0.5em 0;
-            background-image: linear-gradient(
-              135deg,
-              #feb692 10%,
-              #ea5455 100%
-            );
-            p {
-              margin-left: 0.5em;
-            }
-          }
-        }
-      }
-    }
     .ScrollButton {
       font-size: 3rem;
       color: #fff;
@@ -146,74 +68,6 @@ $border: #5f9ea0;
 @media only screen and (min-width: 321px) {
   .Landing_Container {
     padding: 2em;
-    .TopQuote_Container {
-      .TopQuote {
-        margin: 2em 0;
-        padding: 1em 1em 0 1em;
-        text-align: left;
-        height: auto;
-        background: #ece9e6; /* fallback for old browsers */
-        background: -webkit-linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-        border-radius: 1em;
-        box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
-          rgba(0, 0, 0, 0.23) 0px 6px 6px;
-        .Quote {
-          font-style: italic;
-          font-weight: 500;
-          font-size: 1em;
-        }
-        .Reactions {
-          display: flex;
-          justify-content: space-evenly;
-          color: white;
-          padding: 1em 0;
-
-          .Likes {
-            display: flex;
-            width: 50%;
-            padding: 1em 2em;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0.5em 0 0 0.5em;
-            background-image: linear-gradient(
-              135deg,
-              #81fbb8 10%,
-              #28c76f 100%
-            );
-            margin-right: 0.5em;
-            p {
-              margin-left: 0.5em;
-            }
-          }
-          .Dislikes {
-            display: flex;
-            padding: 1em 2em;
-            width: 50%;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0 0.5em 0.5em 0;
-            background-image: linear-gradient(
-              135deg,
-              #feb692 10%,
-              #ea5455 100%
-            );
-            p {
-              margin-left: 0.5em;
-            }
-          }
-        }
-      }
-    }
     .ScrollButton {
       font-size: 3rem;
       color: #fff;
@@ -232,74 +86,6 @@ $border: #5f9ea0;
 @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
   .Landing_Container {
     padding: 2em;
-    .TopQuote_Container {
-      .TopQuote {
-        margin: 2em 0;
-        padding: 1em 1em 0 1em;
-        text-align: left;
-        height: auto;
-        background: #ece9e6; /* fallback for old browsers */
-        background: -webkit-linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-        border-radius: 1em;
-        box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
-          rgba(0, 0, 0, 0.23) 0px 6px 6px;
-        .Quote {
-          font-style: italic;
-          font-weight: 500;
-          font-size: 1em;
-        }
-        .Reactions {
-          display: flex;
-          justify-content: space-evenly;
-          color: white;
-          padding: 1em 0;
-
-          .Likes {
-            display: flex;
-            width: 50%;
-            padding: 1em 2em;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0.5em 0 0 0.5em;
-            background-image: linear-gradient(
-              135deg,
-              #81fbb8 10%,
-              #28c76f 100%
-            );
-            margin-right: 0.5em;
-            p {
-              margin-left: 0.5em;
-            }
-          }
-          .Dislikes {
-            display: flex;
-            padding: 1em 2em;
-            width: 50%;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0 0.5em 0.5em 0;
-            background-image: linear-gradient(
-              135deg,
-              #feb692 10%,
-              #ea5455 100%
-            );
-            p {
-              margin-left: 0.5em;
-            }
-          }
-        }
-      }
-    }
     .ScrollButton {
       font-size: 3rem;
       color: #fff;
@@ -313,74 +99,6 @@ $border: #5f9ea0;
 @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: landscape) {
   .Landing_Container {
     padding: 2em;
-    .TopQuote_Container {
-      .TopQuote {
-        margin: 2em 0;
-        padding: 1em 1em 0 1em;
-        text-align: left;
-        height: auto;
-        background: #ece9e6; /* fallback for old browsers */
-        background: -webkit-linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-        border-radius: 1em;
-        box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
-          rgba(0, 0, 0, 0.23) 0px 6px 6px;
-        .Quote {
-          font-style: italic;
-          font-weight: 500;
-          font-size: 1em;
-        }
-        .Reactions {
-          display: flex;
-          justify-content: space-evenly;
-          color: white;
-          padding: 1em 0;
-
-          .Likes {
-            display: flex;
-            width: 50%;
-            padding: 1em 2em;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0.5em 0 0 0.5em;
-            background-image: linear-gradient(
-              135deg,
-              #81fbb8 10%,
-              #28c76f 100%
-            );
-            margin-right: 0.5em;
-            p {
-              margin-left: 0.5em;
-            }
-          }
-          .Dislikes {
-            display: flex;
-            padding: 1em 2em;
-            width: 50%;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0 0.5em 0.5em 0;
-            background-image: linear-gradient(
-              135deg,
-              #feb692 10%,
-              #ea5455 100%
-            );
-            p {
-              margin-left: 0.5em;
-            }
-          }
-        }
-      }
-    }
     .ScrollButton {
       font-size: 3rem;
       color: #fff;
@@ -394,74 +112,6 @@ $border: #5f9ea0;
 @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: portrait) {
   .Landing_Container {
     padding: 2em;
-    .TopQuote_Container {
-      .TopQuote {
-        margin: 2em 0;
-        padding: 1em 1em 0 1em;
-        text-align: left;
-        height: auto;
-        background: #ece9e6; /* fallback for old browsers */
-        background: -webkit-linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-        border-radius: 1em;
-        box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
-          rgba(0, 0, 0, 0.23) 0px 6px 6px;
-        .Quote {
-          font-style: italic;
-          font-weight: 500;
-          font-size: 1em;
-        }
-        .Reactions {
-          display: flex;
-          justify-content: space-evenly;
-          color: white;
-          padding: 1em 0;
-
-          .Likes {
-            display: flex;
-            width: 50%;
-            padding: 1em 2em;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0.5em 0 0 0.5em;
-            background-image: linear-gradient(
-              135deg,
-              #81fbb8 10%,
-              #28c76f 100%
-            );
-            margin-right: 0.5em;
-            p {
-              margin-left: 0.5em;
-            }
-          }
-          .Dislikes {
-            display: flex;
-            padding: 1em 2em;
-            width: 50%;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0 0.5em 0.5em 0;
-            background-image: linear-gradient(
-              135deg,
-              #feb692 10%,
-              #ea5455 100%
-            );
-            p {
-              margin-left: 0.5em;
-            }
-          }
-        }
-      }
-    }
     .ScrollButton {
       font-size: 3rem;
       color: #fff;
@@ -475,74 +125,6 @@ $border: #5f9ea0;
 @media only screen and (min-width: 1224px) {
   .Landing_Container {
     padding: 2em 40%;
-    .TopQuote_Container {
-      .TopQuote {
-        margin: 2em 0;
-        padding: 1em 1em 0 1em;
-        text-align: left;
-        height: auto;
-        background: #ece9e6; /* fallback for old browsers */
-        background: -webkit-linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-        border-radius: 1em;
-        box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
-          rgba(0, 0, 0, 0.23) 0px 6px 6px;
-        .Quote {
-          font-style: italic;
-          font-weight: 500;
-          font-size: 1em;
-        }
-        .Reactions {
-          display: flex;
-          justify-content: space-evenly;
-          color: white;
-          padding: 1em 0;
-
-          .Likes {
-            display: flex;
-            width: 50%;
-            padding: 1em 2em;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0.5em 0 0 0.5em;
-            background-image: linear-gradient(
-              135deg,
-              #81fbb8 10%,
-              #28c76f 100%
-            );
-            margin-right: 0.5em;
-            p {
-              margin-left: 0.5em;
-            }
-          }
-          .Dislikes {
-            display: flex;
-            padding: 1em 2em;
-            width: 50%;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0 0.5em 0.5em 0;
-            background-image: linear-gradient(
-              135deg,
-              #feb692 10%,
-              #ea5455 100%
-            );
-            p {
-              margin-left: 0.5em;
-            }
-          }
-        }
-      }
-    }
     .ScrollButton {
       font-size: 3rem;
       color: #fff;
@@ -556,74 +138,6 @@ $border: #5f9ea0;
 @media only screen and (min-width: 1824px) {
   .Landing_Container {
     padding: 2em 40%;
-    .TopQuote_Container {
-      .TopQuote {
-        margin: 2em 0;
-        padding: 1em 1em 0 1em;
-        text-align: left;
-        height: auto;
-        background: #ece9e6; /* fallback for old browsers */
-        background: -webkit-linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-        border-radius: 1em;
-        box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
-          rgba(0, 0, 0, 0.23) 0px 6px 6px;
-        .Quote {
-          font-style: italic;
-          font-weight: 500;
-          font-size: 1em;
-        }
-        .Reactions {
-          display: flex;
-          justify-content: space-evenly;
-          color: white;
-          padding: 1em 0;
-
-          .Likes {
-            display: flex;
-            width: 50%;
-            padding: 1em 2em;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0.5em 0 0 0.5em;
-            background-image: linear-gradient(
-              135deg,
-              #81fbb8 10%,
-              #28c76f 100%
-            );
-            margin-right: 0.5em;
-            p {
-              margin-left: 0.5em;
-            }
-          }
-          .Dislikes {
-            display: flex;
-            padding: 1em 2em;
-            width: 50%;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0 0.5em 0.5em 0;
-            background-image: linear-gradient(
-              135deg,
-              #feb692 10%,
-              #ea5455 100%
-            );
-            p {
-              margin-left: 0.5em;
-            }
-          }
-        }
-      }
-    }
     .ScrollButton {
       font-size: 3rem;
       color: #fff;
@@ -638,74 +152,6 @@ $border: #5f9ea0;
   only screen and (min-device-pixel-ratio: 1.5) {
   .Landing_Container {
     padding: 2em;
-    .TopQuote_Container {
-      .TopQuote {
-        margin: 2em 0;
-        padding: 1em 1em 0 1em;
-        text-align: left;
-        height: auto;
-        background: #ece9e6; /* fallback for old browsers */
-        background: -webkit-linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(
-          to right,
-          #ffffff,
-          #ece9e6
-        ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
-        border-radius: 1em;
-        box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px,
-          rgba(0, 0, 0, 0.23) 0px 6px 6px;
-        .Quote {
-          font-style: italic;
-          font-weight: 500;
-          font-size: 1em;
-        }
-        .Reactions {
-          display: flex;
-          justify-content: space-evenly;
-          color: white;
-          padding: 1em 0;
-
-          .Likes {
-            display: flex;
-            width: 50%;
-            padding: 1em 2em;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0.5em 0 0 0.5em;
-            background-image: linear-gradient(
-              135deg,
-              #81fbb8 10%,
-              #28c76f 100%
-            );
-            margin-right: 0.5em;
-            p {
-              margin-left: 0.5em;
-            }
-          }
-          .Dislikes {
-            display: flex;
-            padding: 1em 2em;
-            width: 50%;
-            justify-content: center;
-            align-items: center;
-            border-radius: 0 0.5em 0.5em 0;
-            background-image: linear-gradient(
-              135deg,
-              #feb692 10%,
-              #ea5455 100%
-            );
-            p {
-              margin-left: 0.5em;
-            }
-          }
-        }
-      }
-    }
     .ScrollButton {
       font-size: 3rem;
       color: #fff;
